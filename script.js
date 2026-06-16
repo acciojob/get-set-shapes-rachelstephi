28d4295e24f9cab87c8e90b3fddd0ac54822d319
rachelstephi <87799664+rachelstephi@users.noreply.github.com>
2026-06-17 03:17:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this.width=width;
+		this.height=height;
+	}
+	getWidth(){
+		return this.width;
+	}
+	getHeight(){
+		return this.height;
+	}
+	getArea(){
+		return this.width*this.height;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(side){
+	   super(side,side);
+	}
+	getArea(){
+		return this.side*this.side;
+	}
+	getPerimeter(){
+		console.log(4*this.side);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
